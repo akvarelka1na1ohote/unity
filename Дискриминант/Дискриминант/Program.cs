e378acc75c4cc9043770a71796a88e906871912d
akvarelka1na1ohote <166593870+akvarelka1na1ohote@users.noreply.github.com>
2024-10-11 09:58:47 +0300

    Дискриминант задача

---
+++ Дискриминант/Дискриминант/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+Console.Write("Введите a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double d;
+double x;
+double x1;
+double x2;
+double xi;
+
+if (a == 0)
+{
+    if (b == 0)
+    {
+        if (c == 0)
+        {
+            Console.Write("Решений бесконечно много");
+        }
+        else
+        {
+            Console.Write("Решений нет");
+        }
+    }
+    else
+    {
+        x = -c / b;
+        Console.Write("x=", x);
+    }
+}
+else
+{
+    d = b * b - 4 * a * c;
+
+    if (d >= 0.00000001)
+	{
+        x1 = (-b - Math.Sqrt(d)) / (2 * a);
+        x2 = (-b + Math.Sqrt(d)) / (2 * a);
+        Console.Write($"x1={x1}, x2={x2}");
+    }
+
+    else if (d <= 0.00000001)
+    {
+        x = (-b) / (2 * a);
+        xi = (Math.Sqrt(-d)) / (2 * a);
+        Console.Write($"x1={x}-{xi}*i, x2={x}+{xi}*i");
+    }
+
+    else
+    {
+        x1 = (-b) / (2 * a);
+        Console.Write($"x1={x1}");
+    }
+}

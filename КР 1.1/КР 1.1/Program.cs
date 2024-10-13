2712de126f93f75eed60f51a1b300559e7b72775
akvarelka1na1ohote <166593870+akvarelka1na1ohote@users.noreply.github.com>
2024-10-13 12:15:48 +0300

    Попытка сдать 1 задачу кр

---
+++ КР 1.1/КР 1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Write("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+double sm;
+double s;
+
+sm = x;
+s = x;
+
+for (int i = 3; i < 14; i + 2)
+{
+    //double s;
+    s *= (-1) * (x * x) / (i * (i - 1));
+    //double sm;
+    sm += s;
+}
+Console.Write(sm);
+
+
+
+
+
+
+

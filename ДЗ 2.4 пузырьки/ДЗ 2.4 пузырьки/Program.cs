84a64bf83e08cb7a4ac38fb3a8a4c311e00be4e5
akvarelka1na1ohote <166593870+akvarelka1na1ohote@users.noreply.github.com>
2024-11-14 00:45:07 +0300

    ДЗ 2.4

---
+++ ДЗ 2.4 пузырьки/ДЗ 2.4 пузырьки/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] number = new int[n];
+for (int i = 0; i < number.Length; i++)
+{
+    number[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+public int Func(int n)
+{
+    for (int i = 0; i < n-1 ; i++)
+    {
+        for (int j = 0; j < n-1; j++)
+        {
+            if (number[j] > number[j + 1])
+            {
+                int a = number[j];
+                number[j] = number[j + 1];
+                number[j + 1] = a;
+            }
+        }
+    }
+}

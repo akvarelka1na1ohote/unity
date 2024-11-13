4592e5c12334e1399639d9980eb6b26b75667290
akvarelka1na1ohote <166593870+akvarelka1na1ohote@users.noreply.github.com>
2024-11-14 00:27:03 +0300

    Дз 2.3

---
+++ ДЗ2.3 про 3n+1/ДЗ2.3 про 3n+1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+public int Func(int n)
+{
+    if (n == 1) return 0;
+    else
+    {
+        int a = 0;
+        while (n!=1)
+        {
+            if (n %2 == 0)
+            {
+                n = n / 2;
+                a = a + 1;
+            }
+            else
+            {
+                n = 3 * n + 1;
+                a = a + 1;
+            }
+        }
+        return a;
+    }
+}

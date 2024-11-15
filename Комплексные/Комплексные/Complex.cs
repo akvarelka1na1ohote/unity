5b353db26768ad50ddf4f86bb4cf714a82179843
akvarelka1na1ohote <166593870+akvarelka1na1ohote@users.noreply.github.com>
2024-11-15 11:18:21 +0300

    Комплексные

---
+++ Комплексные/Комплексные/Complex.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    class Complex
+    {
+        double _real;
+        double _image;
+
+        public Complex(double r, double i)
+        {
+            _real = r;
+            _image = i;
+        }
+
+        public static Complex Add(Complex z1, Complex z2)
+        {
+            return new Complex(z1._real + z2._real, z1._image + z2._image);
+        }
+
+        public static Complex operator +(Complex z1, Complex z2)
+        {
+            return Add(z1, z2);
+        }
+
+        public static Complex Substract(Complex z1, Complex z2)
+        {
+            return new Complex(z1._real - z2._real, z1._image - z2._image);
+        }
+
+        public static Complex operator -(Complex z1, Complex z2)
+        {
+            return Substract(z1, z2);
+        }
+
+        public static Complex Multiply(Complex z1, Complex z2)
+        {
+            return new Complex(
+                z1._real * z2._real - z1._image + z2._image,
+                z1._real * z2._image + z2._real * z1._image);
+        }
+
+        public static Complex operator *(Complex z1, Complex z2)
+        {
+            return Multiply(z1, z2);
+        }
+
+        public double Abs()
+        {
+            return Math.Sqrt(_real * _real + _image * _image);
+        }
+
+        public override string ToString()
+        {
+            return $"{_real}+{_image}i";
+        }
+
+        public static double Angle(Complex z1)
+        {
+            return Math.Atan2(z1._image, z1._real);
+        }
+
+        public static Complex Degree(Complex z1,int n)
+        {
+            double r = z1.Abs();
+            double ugol = Angle(z1);
+            r = Math.Pow(r, n);
+            ugol *= n;
+            return new Complex(r* Math.Cos(ugol), r * Math.Sin(ugol));
+
+        }
+
+    }
+}
---
+++ Комплексные/Комплексные/Program.cs
@@ -0,0 +1,18 @@
+﻿using ComplexNumbers;
+
+var z1 = new Complex(1, 0);
+var z2 = new Complex(0, 1);
+
+
+
+Console.WriteLine(z1);
+Console.WriteLine(z2);
+Console.WriteLine(Complex.Add(z1, z2));
+Console.WriteLine(z1 + z2);
+Console.WriteLine(z1.Abs());
+Console.WriteLine(Complex.Angle(z1));
+Console.Write("Введите степень: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Complex.Degree(z1,n));
+
+Console.ReadKey();

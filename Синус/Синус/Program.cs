f255b0cddd85ef6fb95d5a5c899813cbb004a3df
akvarelka1na1ohote <166593870+akvarelka1na1ohote@users.noreply.github.com>
2024-12-18 11:46:03 +0300

    Синус

---
+++ Синус/Синус/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string way = "f.txt";
+
+        using (StreamWriter writing = new StreamWriter(way))
+        {
+            // заголовок "таблицы" (забавно, что в txt))
+            writing.WriteLine("x \t sin(x)");
+
+            for (double x = 0.0; x <= 1.0; x += 0.1)
+            {
+                writing.WriteLine($"{x} \t {Math.Sin(x)}");
+            }
+        }
+
+        Console.WriteLine($"Таблица значений функции sin(x) была успешно записана в файл {way}.");
+    }
+}
